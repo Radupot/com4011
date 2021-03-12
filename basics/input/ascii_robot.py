f8cc5524943c76ef22f3f2b410442b3858b10e79
repl.it user <replituser@example.com>
2021-03-12 10:48:44 +0000

    Added code to display ascii robot with dynamic eyes

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,20 @@
+# Display a robo-pirate! 
+
+print("Enter your character for the eyes")
+eyes = input()
+
+
+print("\t\t\t    ##########")
+print("\t\t\t    #  {} {}   #".format(eyes,eyes))
+print("\t\t\t    #   --   #")
+print("\t\t\t    ##########")
+print("\t\t\t        ##")
+print("\t\t      ##############")
+print("\t\t ?#####            #####> ")
+print("\t\t      # I am beep  # ")
+print("\t\t      # the pirate!#")
+print("\t\t      #            #")
+print("\t\t      ##############")
+print("\t\t         #\t\t#")
+print("\t\t         #\t\t#")
+print("\t\t         |\t\t#")
