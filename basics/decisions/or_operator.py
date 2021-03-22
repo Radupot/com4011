1fba0618f77a1ef1a773aa70ed861da8807356b7
repl.it user <replituser@example.com>
2021-03-22 18:27:38 +0000

    ad.

---
+++ basics/decisions/or_operator.py
@@ -0,0 +1,10 @@
+print("What type of adventure should I have?")
+adv= input()
+
+if ((adv=="scary") or (adv=="short")):
+  print("Entering the dark forest!")
+if ((adv=="safe") or (adv=="long")):
+  print("Taking the safe route!")
+  
+else :
+  print("Not sure which route to take")
