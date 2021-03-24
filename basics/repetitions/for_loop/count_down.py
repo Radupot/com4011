a783c6a6b208d68b6a5458a80d94c2b04bc9b9f5
repl.it user <replituser@example.com>
2021-03-24 19:49:24 +0000

    Added code to demonstrate the use of a simple for loop.

--- basics/repetitions/for_loop/count_down.py
+++ basics/repetitions/for_loop/count_down.py
@@ -3,13 +3,11 @@ print("How far are we from the cave?")
 
 #user input
 distance = int(input())
-steps = 0
 
 #displaying the remaining steps using a for loop
 
 for count in range(distance,0,-1):
-   print("{} steps remaining".format(distance))
-   distance -= 1 
+   print(count, "steps remaining")
    
 print("")
 print("We have reachede the cave!")
---
+++ basics/repetitions/for_loop/range.py
@@ -0,0 +1,12 @@
+print("What level of brightness is required?")
+
+bright= int(input())
+
+print("\nAdjusting brightness...\n")
+
+for count in range(2, bright +1 , 2):
+  print("Beep's brightness level:", "*" * count)
+  print("Bop's brightness level:", "*" * count)
+  print()
+
+print("Adjustments complete!")
